89446fde12d48937e9d809ea6bdff114bc5a2edd
MartenM <marten.struijk@gmail.com>
2024-06-13 23:05:09 +0200

    Initial version

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <script>
-	import RealmBrowser from "./RealmBrowser.svelte";
-	import HighscoreList from "./HighscoreList.svelte";
+
+import RealmBrowser from "./RealmBrowser.svelte";
+import HighscoreList from "./HighscoreList.svelte";
 
 </script>
 
@@ -11,34 +12,17 @@
 
 <div class="container">
 	<div class="row">
-		<div class="col-md-1">
-			<div class="realms-navigator">
-				<div class="realms-navigator-entry">
-					<i class="fa-regular fa-star"></i>
-					Featured
-				</div>
-				<div class="realms-navigator-entry">
-					<i class="fa-regular fa-star"></i>
-					Featured
-				</div>
-			</div>
+		<div class="col-md-5">
+			<RealmBrowser title={"Rated Realms"} subTitle={"Play realms and earn trophies!"} apiUrl={"/api/published_worlds/rated"}/>
+		</div>
+		<div class="col-md-5">
+			<RealmBrowser title={"Published Realms"} subTitle={"Recently published realms by community members."}/>
+		</div>
+		<div class="col-md-2">
+			<HighscoreList></HighscoreList>
 		</div>
 	</div>
 </div>
-
-<!--<div class="container">-->
-<!--	<div class="row">-->
-<!--		<div class="col-md-5">-->
-<!--			<RealmBrowser title={"Rated Realms"} subTitle={"Play realms and earn trophies!"} apiUrl={"/api/published_worlds/rated"}/>-->
-<!--		</div>-->
-<!--		<div class="col-md-5">-->
-<!--			<RealmBrowser title={"Published Realms"} subTitle={"Recently published realms by community members."}/>-->
-<!--		</div>-->
-<!--		<div class="col-md-2">-->
-<!--			<HighscoreList></HighscoreList>-->
-<!--		</div>-->
-<!--	</div>-->
-<!--</div>-->
 
 <style>
 
